7f51461f78f3f25b4ab86bd2e640193e476164f7
zhangzl <zzloo.jit@gmail.com>
2013-03-29 14:48:46 +0800

    add #line direct

---
+++ Buffer.cpp
@@ -0,0 +1,12 @@
+#include "Buffer.hpp"
+#include <iostream>
+
+Buffer::Buffer()
+{
+  std::string name = "";
+  const char* ptr = NULL;
+  const char* ptr_end = NULL;
+  int line_num = 0;
+  int column_num = 0;
+  std::cout << "Buffer line " << line_num << std::endl;
+};
